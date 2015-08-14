8d33a312b504a7257b49d77700742f3c8ed17f48
manjiri <manjiri.kulkarni@jginfosystems.com>
2015-08-14 17:33:21 +0530

    sale_order_task.py str prob removed

--- sale_order_task.py
+++ sale_order_task.py
@@ -1,26 +1,19 @@
+# -*- coding: utf-8 -*-
 from openerp.osv import fields, osv
 
 class sale_order(osv.osv):
     _inherit='sale.order'
     
     def _prepare_invoice(self, cr, uid, order, lines, context=None):
-        print "sale order ---------------------------",order
         invoice_vals=super(sale_order,self)._prepare_invoice(cr,uid,order,lines,context)
-        print "invoice_vals----------------------------------------------",invoice_vals
         additional_notes=""
         i=1
         total_tasks=order.associated_tasks
-        print "total tasks----------------------------------",total_tasks
         
         for task_obj in total_tasks:
-            print "task_obj.name--------------------",task_obj.name
-            print "iin for iiiiiiiiiiiiiiiiiiiiiiiiiiiii-----------",i
-            additional_notes+= "\n"+str(i)+"."+str(task_obj.name)+"("+str(task_obj.sequence1)+")"
-            print "addtional notes------------------------------",additional_notes
+            additional_notes+= "\n"+str(i)+"."+ (task_obj.name)+"("+str(task_obj.sequence1)+")"
             i+=1
-        print "final comments------------------------------",additional_notes
         invoice_vals['comment']=additional_notes
-        print "invoice_vals 22222222----------------------------------------------",invoice_vals
         return invoice_vals
     
     def _associated_tasks_with_saleorder(self, cr, uid, ids, field_name, arg, context=None):
